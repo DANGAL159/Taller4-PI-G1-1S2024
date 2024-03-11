b9d367427428967ca9bf7eef94d3eb15102fa657
Matius-Noriega <3154436181301@ingenieria.usac.edu.gt>
2024-03-11 14:19:07 -0600

    Se añade la base de datos

--- connection.js
+++ connection.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     user: process.env,DB_USERNAME,
     password: process.env,DB_PASSWORD,
     database: process.env, DB_NAME
-})
+});
 
 connection.connect((err) => {
     if(!err){
