1a21f6fcdab7e360bf1243e07d237300f5432ef5
JesicaColaj <201516997@ingenieria.usac.edu.gt>
2024-03-11 22:42:12 -0600

    conexiones a mysql para el registro

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,14 +3,20 @@ const connected = require('../connection');
 const router = express.Router();
 
 
-router.post('/signup', (req, res)=> {
+router.post('/signup', (req, res) => {
     let user = req.body;
     query = "select email, password, role, status from user where email=?"
     connection.query(query,[user.email],(err, results)=> {
         if(!err){
             if(results.length <= 0){
                 query = "insert into users(name,contact, email, password, status, role)value (?,?,?,?, 'false', 'user')"
-                connection.query(query, [user.name, user.contact, user.email, user.password],(err, results))
+                connection.query(query, [user.name, user.contact, user.email, user.password],(err, results) => {
+                    if(!err){
+                        return res.status(200).json({message: "Successfully registered."});
+                    } else {
+                        return res.status(500).json(err);
+                    }
+                })
             } else {
                 return res.status(400).json({message: "Emaily already exist."});
             } 
